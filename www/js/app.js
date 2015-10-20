beb591b4191a574419c9936d435e49c0c2526977
rcorbeil <rcorbeil@softstackfactory.com>
2015-10-20 21:33:56 +0000

    before adding Register

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers', 'RESTConnection'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -28,6 +28,11 @@ angular.module('starter', ['ionic'])
     .state('login',{
     url:'/login',
     templateUrl:'templates/login.html',
+    controller: 'LoginCtrl'
+    })
+    .state('lobby', {
+    url: '/lobby',
+    templateUrl: 'templates/lobby.html',
     })
     .state('register',{
     url:'/register',
